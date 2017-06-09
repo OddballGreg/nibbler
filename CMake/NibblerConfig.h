ca2739367e040bf453df941f193d93a9e84c365e
cdebruyn <debruyn.sarel@gmail.com>
2017-06-09 14:10:35 +0200

    CMake coming together now.

---
+++ CMake/NibblerConfig.h
@@ -0,0 +1,7 @@
+// the configured option and settings for Nibbler
+#define Nibbler_VERSION_MAJOR 1
+#define Nibbler_VERSION_MINOR 0
+
+/* #undef USE_OPENGL */
+/* #undef USE_VTK */
+/* #undef USE_WEBGL */
