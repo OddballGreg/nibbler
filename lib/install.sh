2a9b2f636ccd27f7c2f8f7cd3b87b9bd66745774
cdebruyn <debruyn.sarel@gmail.com>
2017-06-14 14:31:34 +0200

    Commiting new gitignore rules.

--- lib/install.sh
+++ lib/install.sh
@@ -8,14 +8,27 @@ LIB_VTK="./lib/VTK"
 VTK="VTK"
 LIB="./lib"
 
+LIB_OGRE="./lib/Ogre3D"
+OGRE="Ogre3D"
+
 if [ ! -d "$LIB_VTK" ]; then
 	echo "Preparing to install VTK..."
 	echo "Begin downloading VTK from GitHub...."
-	#git clone https://github.com/Kitware/VTK.git VTK 
+	git clone https://github.com/Kitware/VTK.git $VTK 
 	echo "Busy creating CMake components"
-	#cmake -D CMAKE_C_COMPILER="/usr/bin/clang" -D CMAKE_CXX_COMPILER="/usr/bin/clang++" "./VTK/CMakeLists.txt"
+	cmake -D CMAKE_C_COMPILER="/usr/bin/clang" -D CMAKE_CXX_COMPILER="/usr/bin/clang++" "./VTK/CMakeLists.txt"
 	echo "Beginning to install VTK...."
 	make -C "$VTK" install
 	mv "$VTK" "$LIB"
 fi
 
+if [ ! -d "$LIB_OGRE"]; then
+	echo "preparing to install Ogre3D..."
+	echo "Begin downloading Ogre3D from Bitbucket...."
+	git clone https://bitbucket.org/sinbad/ogre $OGRE
+	echo "Busy creating CMake components"
+	cmake -D CMAKE_C_COMPILER="/usr/bin/clang" -D CMAKE_CXX_COMPILER="usr/bin/clang++" "./Ogre3D/CMakeLists.txt"
+	echo "Beginning to install Ogre3D...."
+	make -C "$OGRE" install
+	mv "$OGRE" "$LIB"
+fi
