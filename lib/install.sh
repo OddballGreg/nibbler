b30651fb5eb470aa91cd3d46940f2d7770bb37a1
Cameron DE BRUYN <cdebruyn@e5r10p5.jnb.42.fr>
2017-06-15 02:54:38 +0200

    lala

--- lib/install.sh
+++ lib/install.sh
@@ -4,6 +4,13 @@ clear
 
 echo "Checking to see if any graphic libraries are installed..."
 
+echo "Now before you start. I will tell you to remember to install the"
+echo "dependencies. Remember the fucking dependencies. I will not tell"
+echo "you what they are. But remember the FUCKING dependencies !!!!!"
+echo ""
+
+sleep 
+
 LIB_VTK="./lib/VTK"
 VTK="VTK"
 LIB="./lib"
@@ -25,10 +32,11 @@ fi
 if [ ! -d "$LIB_OGRE" ]; then
 	echo "preparing to install Ogre3D..."
 	echo "Begin downloading Ogre3D from Bitbucket...."
-	#hg clone https://bitbucket.org/sinbad/ogre $OGRE
+	hg clone https://bitbucket.org/sinbad/ogre $OGRE
 	echo "Busy creating CMake components"
 	cmake -D CMAKE_C_COMPILER="/usr/bin/clang" -D CMAKE_CXX_COMPILER="/usr/bin/clang++" "./Ogre3D/CMakeLists.txt"
-	echo "Beginning to install Ogre3D...."
-	make -C "$OGRE" install
+	echo "Beginning to install Ogre3D......"
+	make -C "$OGRE" install/local
 	mv "$OGRE" "$LIB"
+	echo "Finish install Ogre3D...."
 fi
