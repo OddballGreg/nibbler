b3cbc64f232d37b917434f9c9f86d8bc80029111
Kalilo <kalilohansmann@gmail.com>
2017-06-17 13:06:01 +0200

    Begun working on ncurses window

---
+++ ncurses/NcursesWindow.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   NcursesWindow.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/17 11:35:35 by khansman          #+#    #+#             */
+/*   Updated: 2017/06/17 11:35:37 by khansman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NCURSES_WINDOW_HPP
+ # define NCURSES_WINDOW_HPP
+
+# include "../shared/IDisplay.hpp"
+
+class NcursesWindow : public IDisplay{
+public:
+	NcursesWindow(void);
+	virtual ~NcursesWindow(void);
+
+	NcursesWindow(const NcursesWindow &obj);
+	virtual NcursesWindow operator = (const NcursesWindow &obj);
+
+	virtual void		drawMap(MAP map);
+	virtual void		drawScore(int score);
+	virtual void		drawPause(void);
+	virtual void		drawGameOver(void);
+
+	virtual	void		initWindow(void);
+	virtual void		exitWindow(void);
+
+	virtual Direction	getDirection(void);
+	virtual int			getWindowSize(void);
+
+private:
+	Coord		_size;
+	Direction	_direction;
+	MAP			_map;
+
+};
+
+#endif
