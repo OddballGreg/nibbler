847d184bdb329e30ee63beb62636af3959a22859
Kalilo <kalilohansmann@gmail.com>
2017-06-20 07:49:50 +0200

    Added missing endwin

--- environ/ncurses/NcursesWindow.cpp
+++ environ/ncurses/NcursesWindow.cpp
@@ -29,7 +29,7 @@ NcursesWindow::~NcursesWindow(void) {
 	listen = false;
 	// _map.clear();
 
-	if (_win)
+	if (_win != false)
 		delwin(this->_win);
 
 	// if (_panel)
@@ -168,6 +168,7 @@ void		NcursesWindow::exitWindow(void) {
 		delwin(this->_win);
 		this->_win = NULL;
 	}
+	endwin();
 	// if (_panel) {
 	// 	del_panel(this->_panel);
 	// 	this->_panel = NULL;
