0588ea8558d5a1ea51de05ccf359ddf2b8fbb034
Kalilo <kalilohansmann@gmail.com>
2017-06-23 10:39:01 +0200

    Added boost for argument parsing

--- host/main.cpp
+++ host/main.cpp
@@ -1,27 +1,55 @@
+#define MAIN_FILE
 #include "./main.hpp"
 
 //void puts(std::string message) { std::cout << message << std::endl; }
 
-void			handleArgs(int ac, char **av) {
-	if (ac <= 1) {
-		std::cout << "Using Default Sizes" << std::endl;
-	}
-	else if (ac % 2 == 0) {//rethink
-		std::cout << "Invalid arguments. Usage [-v 0..9] [-h 0..100] [-w 0..100] [-a]" << std::endl;
-		exit(0);
-	}
-	else {
-		for (int k = 1; k < ac; k++) {
-			(void)av;
+void			parseArgs(int ac, char **av) {
+	try {
+		boost::program_options::options_description	desc("Options");
+
+		desc.add_options()
+		("help", "Print out help messages")
+		("verbose,v", boost::program_options::value<int>(&g_verbosity), "runs logs with a verbosity of 0 to 10")
+		("height,h", boost::program_options::value<int>(&g_height)->required(), "sets the window height")
+		("width,w", boost::program_options::value<int>(&g_width)->required(), "sets the window width")
+		("ai,a", "enables the AI")
+		("verse_ai,b", "allows the player to play against the AI");
+
+		boost::program_options::variables_map	vm;
+
+		try {
+			boost::program_options::store(boost::program_options::parse_command_line(ac, av, desc), vm);
+
+			if (vm.count("help")) {
+				std::cout << "Nibbler is a version of the classical snake game, which allows for dynamic graphics libraies to be loaded and used as interfaces." << std::endl;
+				std::cout << std::endl << desc << std::endl;
+				exit(0);
+			}
+
+			boost::program_options::notify(vm);
+
+			if (g_height < 10 || g_height > 100)
+				throw boost::program_options::error("height can not be greater than 100, or less than 10");
+			if (g_width < 10 || g_width > 100)
+				throw boost::program_options::error("width can not be greater than 100, or less than 10");
 		}
+		catch (boost::program_options::error& e) {
+			std::cerr << "ERROR: " << e.what() << std::endl;
+			exit(0);
+		}
+	}
+	catch(std::exception& e) {
+		std::cerr << "Exception reached when parsing paramiters" << std::endl;
+		exit(0);
 	}
 }
 
-int				main( int argc, char **argv )
+int				main( int ac, char **av )
 {
-	if (argc > 1)
-		if (std::atoi(argv[1]) >= NONE && std::atoi(argv[1]) <= ALL)
-			logger.setVerbosity(std::atoi(argv[1]));
+	parseArgs(ac, av);
+	// if (argc > 1)
+	// 	if (std::atoi(argv[1]) >= NONE && std::atoi(argv[1]) <= ALL)
+	// 		logger.setVerbosity(std::atoi(argv[1]));
 
 	logger.log("", CRITICAL);
 	logger.log("----------------------------------------------------", CRITICAL);
--- host/main.hpp
+++ host/main.hpp
@@ -1,13 +1,31 @@
 #ifndef MAIN_HPP
 # define MAIN_HPP
 
-#include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <vector>
-#include <fstream>
-#include <fcntl.h>
-#include <stdio.h>
+# include <iostream>
+# include <string>
+# include <stdlib.h>
+# include <vector>
+# include <fstream>
+# include <fcntl.h>
+# include <stdio.h>
+
+# include <boost/program_options.hpp>
+
+# ifdef MAIN_FILE
+
+bool			g_ai_flag = false;
+int				g_height = 20;
+int				g_width = 20;
+int				g_verbosity = 10;
+
+# else
+
+extern bool		g_ai_flag;
+extern int		g_height;
+extern int		g_width;
+extern int		g_verbosity;
+
+# endif
 
 extern "C" {
 	#include <dlfcn.h>
